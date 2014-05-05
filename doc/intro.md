c777497c0278985fdd4b8ac8fb07dc7136b0d4bf
Bobby Eickhoff <bobby.eickhoff2@kroger.com>
2014-05-05 11:50:57 -0400

    Initial project state

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dojo.week30
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/dojo/week30_test.clj
@@ -0,0 +1,7 @@
+(ns dojo.week30-test
+  (:require [clojure.test :refer :all]
+            [dojo.week30 :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
